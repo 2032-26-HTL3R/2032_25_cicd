cea5a1548b59f55675b6d7d0835149060e47c94b
Efe Aydin <aydnefe1414@gmail.com>
2026-03-11 16:05:47 +0100

    feat: Nötigen Dateien erstellt

---
+++ py_cicd/wc1.py
@@ -0,0 +1,24 @@
+__author__ = "Efe Aydin"
+__task__ = "SEW4/07/01"
+__date__ = "02.03.2026"
+__version__ = "1.2.1"
+__license__ = "GNU GPLv3"
+__status__ = "Released"
+
+def count(text):
+    """
+    Zählt die Anzahl der Wörter in einem gegebenen Text
+    :param text:
+    :return:
+
+    >>> count("eins")
+    1
+    >>> count("eins")
+    1
+    >>> count("ein erster Text")
+    3
+    >>> count("eins <html> und mehr </html>")
+    2
+    """
+
+
